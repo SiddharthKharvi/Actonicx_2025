5c9759dd6610a8b5925790aba140c5c865f22702
Siddharth Kharvi <Siddharthkharvi69756@gmail.com>
2024-11-27 21:47:25 +0530

    project 1

--- PRO_001/PRO_001.ino
+++ PRO_001/PRO_001.ino
@@ -1,9 +1,30 @@
+#define Motor 9
+String recievedData = "";
+
+int Speed = 0; // 0, 50, 150, 250
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available()) {
+    recievedData = Serial.readString();
+    if (recievedData == "STOP") {
+      Speed = 0;
+    }
+    if (recievedData == "MEDIUM") {
+      Speed = 50;
+    }
+    if (recievedData == "HIGH") {
+      Speed = 150;
+    }
+    if (recievedData == "SLOW" || recievedData == "START") {
+      Speed = 250;
+    }
+    analogWrite(9, Speed);
+  }
+  delay(10);
 }
